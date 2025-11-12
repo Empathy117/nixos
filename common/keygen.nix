ad02c8e505ad11b9c95ec75dc278ea56acd61fc2
empathy <empathyyiyiqi@gmail.com>
2025-11-12 17:04:25 +0800

    fix:typo & some silly mistake(if you don't know don't edit it...)

--- common/keygen.nix
+++ common/keygen.nix
@@ -6,15 +6,15 @@
   home.activation.ensureSshKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     set -eu
     umask 077
-    if [ ! -f "$HOME/.ssh/id_ssh_empathy117" ]; then
+    if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
       mkdir -p "$HOME/.ssh"
-      chmod 700 "$HOME/.shh"
-      ssh-keygen -t ssh_empathy117 -N "" -C "${config.home.user.name}@$(hostname)-hm" \
-        -f "$HOME/.ssh/id_ssh_empathy117"
+      chmod 700 "$HOME/.ssh"
+      ssh-keygen -t ed25519 -N "" -C "${config.home.username}@$(hostname)-hm" \
+        -f "$HOME/.ssh/id_ed25519"
       echo
-      echo "Generated SSH key at $HOME/.ssh/id_ssh_empathy117"
+      echo "Generated SSH key at $HOME/.ssh/id_ed25519"
       echo "Public key:"
-      cat "$HOME/.ssh/id_ssh_empathy117.pub"
+      cat "$HOME/.ssh/id_ed25519.pub"
       echo
     fi
   '';
