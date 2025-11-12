9dae38f07fbefe32efd1aa34eb9838f30f2edc3d
empathy <empathyyiyiqi@gmail.com>
2025-11-12 20:41:21 +0800

    fix:some mistake on keygen.nix

--- common/keygen.nix
+++ common/keygen.nix
@@ -1,21 +1,21 @@
 # ~/nixos/common/keygen.nix
 
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   home.activation.ensureSshKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     set -euo pipefail
     umask 077
-    key = "$HOME/.ssh/id_ed25519"
+    key="$HOME/.ssh/id_ed25519"
 
     if [ ! -f "$key" ]; then
       mkdir -p "$HOME/.ssh"
       chmod 700 "$HOME/.ssh"
       
-      host = "$(cat /proc/sys/kernel/hostname 2>/dev/null || echo unknown)"
+      host="$(cat /proc/sys/kernel/hostname 2>/dev/null || echo unknown)"
 
-      "${pkgs.openssh}/bin/ssh-keygen" -t ed25519 - N "" \
-        -C "${config.home.username}@$(host)-hm" -f "$key"
+      "${pkgs.openssh}/bin/ssh-keygen" -t ed25519 -N "" \
+        -C "${config.home.username}@$host-hm" -f "$key"
 
       echo
       echo "Generated SSH key at $HOME/.ssh/id_ed25519"
