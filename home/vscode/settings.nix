cebba15ee39649260e0acaa40dfff138d1a9ea29
empathy <empathyyiyiqi@gmail.com>
2025-11-14 21:55:35 +0800

    fix: modify vscode settings

--- home/vscode/settings.nix
+++ home/vscode/settings.nix
@@ -2,18 +2,22 @@ let
   workspaceFolderLiteral = "$" + "{workspaceFolder}";
 in
 {
-  "editor.formatOnSave" = true;
+  "editor.formatOnSave" = false;
   "git.autofetch" = true;
   "nix.enableLanguageServer" = true;
-  "nix.serverPath" = "nixd"; # or "nil", or ["executable", "argument1", ...]
+  "nix.serverPath" = "~/.nix-profile/bin/nixd";
   # LSP config can be passed via ``nix.serverSettings.{lsp}`` as shown below.
   "nix.serverSettings" = {
     # check https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md for all nixd config
     "nixd" = {
+      "diagnostics" = {
+        "strictEval" = false;
+      };
       "formatting" = {
         "command" = [ "nixfmt" ];
       };
       "options" = {
+        "autoDiscover" = false;
         # By default, this entry will be read from `import <nixpkgs> { }`.
         # You can write arbitrary Nix expressions here, to produce valid "options" declaration result.
         # Tip: for flake-based configuration, utilize `builtins.getFlake`
