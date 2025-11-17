09ce37707fec9ed0765a2f83681a2a5dfa251612
empathy <empathyyiyiqi@gmail.com>
2025-11-17 18:39:14 +0800

    fix: comment ghproxy

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,8 +4,8 @@ _: {
     extraConfig = {
       core.sshCommand = "ssh -4";
       net.defaultAddressFamily = "inet";
-      # url."ssh://git@github.com/".insteadOf = "https://github.com/";
-      url."https://gh-proxy.com/https://github.com/".insteadOf = "https://github.com/";
+      url."ssh://git@github.com/".insteadOf = "https://github.com/";
+      # url."https://gh-proxy.com/https://github.com/".insteadOf = "https://github.com/";
       url."ssh://git@gitee.com/".insteadOf = "https://gitee.com/";
       user.email = "empathyyiyiqi@gmail.com";
       user.name = "empathy";
