7c0f9e17e82bc5cd5ae11debd5f6e2f47374a771
empathy <empathyyiyiqi@gmail.com>
2025-11-17 18:05:13 +0800

    add: ssh public key & vlock

--- hosts/devbox.nix
+++ hosts/devbox.nix
@@ -29,6 +29,10 @@
     ];
     shell = pkgs.zsh;
     description = "Primary development account";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrO/0OgAxwADiPm93IrC9Y87Kfc6pr1OhkbD+bF77ge empathy@DyldadeMacBook-Pro.local"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXam0l+NkYNEisRt9nYAIR/cfCpMONzX+tVJpU7TjJm empathyyiyiqi@gmail.com"
+    ];
   };
 
   security.sudo.wheelNeedsPassword = false;
@@ -37,6 +41,7 @@
     git
     htop
     docker
+    vlock
   ];
 
   virtualisation.docker.enable = true;
