37f5cd14286a01442e8b2b4c5e9acfb9c60a2c85
empathy <empathyyiyiqi@gmail.com>
2025-11-25 16:45:06 +0800

    feat: add ssh key

--- modules/system/git-users.nix
+++ modules/system/git-users.nix
@@ -3,9 +3,10 @@
   # 公共 Git 用户的 SSH 公钥配置
   # 在这里集中管理所有可以通过 git@HOST 访问 /srv/git 的用户 key
   users.users.git.openssh.authorizedKeys.keys = [
-    # 现有的两把 key（来自 devbox 配置），按需追加同事 key
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJrO/0OgAxwADiPm93IrC9Y87Kfc6pr1OhkbD+bF77ge empathy@DyldadeMacBook-Pro.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPXam0l+NkYNEisRt9nYAIR/cfCpMONzX+tVJpU7TjJm empathyyiyiqi@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFW4vyHhzj1PHx98OIpQIZVAi6x8zmXyAbJZpJSY3Z0s 2454422368@qq.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKygYCXj2VS1KSZfPwqInGlXB7nwOcVXabDzIm2JWGce 1137380664@qq.com"
   ];
 }
 
