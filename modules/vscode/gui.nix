d49a56c4b5bd4c9bdaa934b68aecfb6c1bf397b9
Empathy117 <empathyyiyiqi@gmail.com>
2026-01-08 23:42:02 +0800

    fix(vscode): support home-manager vscode profiles

--- modules/vscode/gui.nix
+++ modules/vscode/gui.nix
@@ -1,5 +1,7 @@
 {
   config,
+  lib,
+  options,
   pkgs,
   ...
 }:
@@ -7,22 +9,38 @@
 let
   sharedSettings = config.shared.vscode.userSettings;
   sharedExtensions = config.shared.vscode.extensions;
+  hasVscodeProfiles = lib.hasAttrByPath [
+    "programs"
+    "vscode"
+    "profiles"
+  ] options;
 in
 {
   imports = [
     ./base.nix
   ];
 
-  programs.vscode = {
-    enable = true;
-    package =
-      (pkgs.vscode-with-extensions.override {
-        vscodeExtensions = sharedExtensions;
-      }).overrideAttrs
-        (_: {
-          pname = "vscode";
-          inherit (pkgs.vscode) version;
-        });
-    userSettings = sharedSettings;
-  };
+  config = lib.mkMerge [
+    {
+      programs.vscode = {
+        enable = true;
+        package =
+          (pkgs.vscode-with-extensions.override {
+            vscodeExtensions = sharedExtensions;
+          }).overrideAttrs
+            (_: {
+              pname = "vscode";
+              inherit (pkgs.vscode) version;
+            });
+      };
+    }
+
+    (lib.mkIf hasVscodeProfiles {
+      programs.vscode.profiles.default.userSettings = sharedSettings;
+    })
+
+    (lib.mkIf (!hasVscodeProfiles) {
+      programs.vscode.userSettings = sharedSettings;
+    })
+  ];
 }
