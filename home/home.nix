30863e65ecd9388878cdf5e7a8d1fdc1f1046f3c
Empathy117 <empathyyiyiqi@gmail.com>
2026-01-11 17:27:04 +0800

    feat: opencode settings

--- home/home.nix
+++ home/home.nix
@@ -13,6 +13,22 @@ _: {
     ../modules/home/zsh.nix
     ../modules/home/direnv.nix
     ../modules/home/nodejs
+    ../modules/home/opencode.nix
   ];
+
+  programs.opencode = {
+    enable = true;
+    oh-my-opencode = {
+      enable = true;
+      settings = {
+        google_auth = true;
+      };
+    };
+    settings = {
+      plugin = [
+        "oh-my-opencode"
+      ];
+    };
+  };
 
 }
